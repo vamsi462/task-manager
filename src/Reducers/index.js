8211d892668b421611b667899a27c8013775fd2e
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-11 17:11:34 +0530

    switch case version for the action type

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,42 +1,65 @@
-import { act } from "react-dom/test-utils";
-import { EDIT_TASK } from "../actions/types";
+import {act} from "react-dom/test-utils";
+import {EDIT_TASK} from "../actions/types";
 
 const intialState = [
-    {
-        id:1,
-        title:"Learn Redux",
-        description:"Let's learn react with redux today!!!",
-        status: "In Progress"
-    },
-    {
-        id: 2,
-        title: "Build react-redux app",
-        description: "Let's Build task manager today with redux !!!",
-        status: "Done"
-    },
+  {
+    id: 1,
+    title: "Learn Redux",
+    description: "Let's learn react with redux today!!!",
+    status: "In Progress",
+  },
+  {
+    id: 2,
+    title: "Build react-redux app",
+    description: "Let's Build task manager today with redux !!!",
+    status: "Done",
+  },
 
-    {
-        id: 3,
-        title: "Let's Learn MERN stack ",
-        description: "Let's Build E-commerce with MERN stack!!!",
-        status: "Not Started"
-    }
-]
+  {
+    id: 3,
+    title: "Let's Learn MERN stack ",
+    description: "Let's Build E-commerce with MERN stack!!!",
+    status: "Not Started",
+  },
+];
 
-const tasks =(state={tasks:intialState},action)=>{
-    if(action.type===EDIT_TASK){
-        const {payload}=action;
-        return{
-            tasks: state.tasks.map((task) => {
-                if(task.id===payload.id){
-                    return Object.assign({},task,payload.params);
-                }
-                return task;
-            }),
-            
-        };
+const tasks = (state = {tasks: intialState}, action) => {
+        /***************************************************************************
+                            if-else version 
+        ***************************************************************************/
+  // if(action.type===EDIT_TASK){
+  //     const {payload}=action;
+  //     return{
+  //         tasks: state.tasks.map((task) => {
+  //             if(task.id===payload.id){
+  //                 return Object.assign({},task,payload.params);
+  //             }
+  //             return task;
+  //         }),
+  //     };
+  // }
+  // return state
+
+  
+        /***************************************************************************
+                            switch version 
+         ***************************************************************************/
+  const {payload} = action;
+  switch (action.type) {
+    case EDIT_TASK: {
+      return {
+        tasks: state.tasks.map((task) => {
+          if (task.id === payload.id) {
+            return Object.assign({}, task, payload.params);
+          }
+          return task;
+        }),
+      };
     }
-    return state
-}
+
+    default:
+      return state;
+  }
+};
 
 export default tasks;
